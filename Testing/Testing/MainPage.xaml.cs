b871ace8a4818007f55827d9915bfec5d52903ab
Marcin Wesel <marcin.wesel@pl.abb.com>
2018-05-17 17:39:54 +0200

    Initial commit

---
+++ Testing/Testing/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Testing
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+        }
+
+        internal async void btnNext_Clicked(object sender, EventArgs e)
+        {
+           var url = entryUrl.Text;
+           await Navigation.PushAsync(new NavigationPage(new SecondPage(url)));
+        }
+
+    }
+}
---
+++ Testing/Testing/SecondPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Testing
+{
+	public partial class SecondPage : ContentPage
+	{
+        private string _url;
+
+		public SecondPage(string url)
+		{
+			InitializeComponent();
+            _url = url;
+            //wvWeb.Source = url;
+        }
+
+        internal async void btnClicked(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(_url);
+                var status = response.StatusCode;
+            }
+        }
+    }
+}
